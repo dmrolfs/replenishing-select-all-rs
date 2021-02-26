efcd3a43a9cd946b7dd06a1d53c79ffc4a92addb
Damon Rolfs <drolfs@gmail.com>
2021-02-26 11:07:17 -0800

    minor cleanup before introducing actor api

--- src/merge_n.rs
+++ src/merge_n.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use futures::future::{BoxFuture, FutureExt};
+use futures::future::{self, BoxFuture, FutureExt};
 use std::fmt;
 use std::sync::Arc;
 use tokio::sync::mpsc;
@@ -61,54 +61,52 @@ where
         fields(name=%self.name),
     )]
     async fn run(&mut self) {
-        let mut targets = self.initial_targets().await;
+        let mut active_inlets = self.initialize_active_inlets().await;
         let outlet = &self.outlet;
 
-        while !targets.is_empty() {
-            let select_targets = targets;
-            tracing::info!(nr_targets=%select_targets.len(), "1.selecting from targets");
+        while !active_inlets.is_empty() {
+            let available_inlets = active_inlets;
+            tracing::info!(nr_available_inlets=%available_inlets.len(), "1.selecting from active inlets");
 
-            let (inlet_value, target_idx, remaining) = futures::future::select_all(select_targets).await;
-            let (inlet_idx, value) = inlet_value;
+            let ((inlet_idx, value), target_idx, mut remaining_inlets) = future::select_all(available_inlets).await;
             let is_active = value.is_some();
 
             let handle_target_span = tracing::info_span!(
-                "handle selected recv target",
+                "2.handle selected recv item",
                 ?value,
                 %is_active,
                 %inlet_idx,
                 %target_idx,
-                nr_remaining=%remaining.len(),
+                nr_remaining=%remaining_inlets.len(),
             );
             let _handle_target_guard = handle_target_span.enter();
-
-            tracing::info!("2.selected next item.");
 
             if let Some(item) = value {
                 let send_active_span = tracing::info_span!("3.send item to outlet", ?item);
                 let _send_active_guard = send_active_span.enter();
 
                 outlet.send(item).await.expect("failed to send to outlet");
-                tracing::info!("3.1.item sent to outlet.");
             }
 
-            let mut remaining_targets = remaining;
-            tracing::info!(nr_remaining=%remaining_targets.len(), %is_active, "after send");
+            tracing::info!(nr_remaining=%remaining_inlets.len(), %is_active, "after send");
 
             if is_active {
-                let run_active_span = tracing::info_span!("4.replenish targets");
+                let run_active_span = tracing::info_span!(
+                    "4.replenish active inlets",
+                    nr_available_inlets=%remaining_inlets.len()
+                );
                 let _run_active_guard = run_active_span.enter();
 
                 if let Some(inlet) = self.inlets.lock().await.get(inlet_idx) {
                     let rep = MergeN::replenish_inlet_recv(inlet_idx, inlet.clone()).boxed();
-                    remaining_targets.push(rep);
+                    remaining_inlets.push(rep);
                     // remaining_targets.insert(idx, rep);
-                    tracing::info!(nr_targets=%remaining_targets.len(), "4.1.targets replenished.");
+                    tracing::info!(nr_available_inlets=%remaining_inlets.len(), "4.1.active_inlets replenished.");
                 }
             }
 
-            targets = remaining_targets;
-            tracing::info!(nr_remaining=%targets.len(), %is_active, ">>>> loop bottom <<<<");
+            active_inlets = remaining_inlets;
+            tracing::info!(nr_remaining=%active_inlets.len(), %is_active, ">>>> loop bottom <<<<");
         }
         // loop {
         //     tokio::select! {
@@ -148,16 +146,16 @@ impl<T> MergeN<T>
 where
     T: fmt::Debug + Send + Sync + 'static,
 {
-    #[tracing::instrument(level = "info", name = "initial targets", skip(self))]
-    async fn initial_targets<'a>(&self) -> Vec<BoxFuture<'a, (usize, Option<T>)>> {
+    #[tracing::instrument(level = "info", skip(self))]
+    async fn initialize_active_inlets<'a>(&self) -> Vec<BoxFuture<'a, (usize, Option<T>)>> {
         let inlets = self.inlets.lock().await;
-        let mut targets = Vec::with_capacity(inlets.len());
+        let mut active_inlets = Vec::with_capacity(inlets.len());
         for (idx, inlet) in inlets.iter().enumerate() {
             let rep = MergeN::replenish_inlet_recv(idx, inlet.clone()).boxed();
-            targets.push(rep);
+            active_inlets.push(rep);
         }
 
-        targets
+        active_inlets
     }
 
     #[tracing::instrument(level = "info", name = "replenish inlet recv", skip(inlet), fields(inlet_idx=%idx))]
